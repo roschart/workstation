a2faca1be6451d004845e3bc728cdd1c8a20151e
Jose Luis Dominguez Balirac <jl.balirac@payvision.com>
2021-02-11 15:17:12 +0100

    ssh a maquinas de hyperion configurado

---
+++ bootstrap.sh
@@ -0,0 +1,1 @@
+gopass show hyperion/linux/golden-image/rhel/hyperion id_rsa > hyperion.priv
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,15 @@ if [ -f "$FILE" ]; then
   cp $FILE /root/.ssh/id_rsa
   chmod 600 /root/.ssh/id_rsa
   ssh-keyscan github.com >> ~/.ssh/known_hosts
+  echo 'IdentityFile ~/.ssh/id_rsa' >> ~/.ssh/config
+fi
+
+FILE=/tmp/hyperion.priv
+if [ -f "$FILE" ]; then
+  mkdir -p /root/.ssh
+  cp $FILE  /root/.ssh/hyperion.priv
+  chmod 600 /root/.ssh/hyperion.priv
+  echo 'IdentityFile ~/.ssh/hyperion.priv' >> ~/.ssh/config
 fi
 
 # if no command the execute ash as default
