51973275adf64766ea2f370157cde110f8188ef1
jl.balirac <jl.balirac@payvision.com>
2021-05-24 17:00:09 +0200

    El dockerfile se genera, pero hay problemas con gopass

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,4 @@
-gopass show hyperion/linux/golden-image/rhel/hyperion id_rsa > hyperion.priv
-cp ~/.kube/config config
+gopass show hyperion/linux/golden-image/rhel/hyperion id_rsa > id_rsa_hyperion
+gopass show hyperion/service_account/hyperion.priv.gpg > hyperion.priv.gpg 
+gopass show  hyperion/atlas/kubernetes/kubeconfig > config
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,37 @@ if [ -f "$FILE" ]; then
   chmod 600 /root/.ssh/id_rsa
   ssh-keyscan github.com >> ~/.ssh/known_hosts
   echo 'IdentityFile ~/.ssh/id_rsa' >> ~/.ssh/config
+
+  # Import private hyperion key and trust
+  # Requirements: have get the hyerion private key from gopass
+  # and mount in tme docker run -v $PWD/hyperion.priv.gpg:/tmp/hyperion.priv.gpg
+  # gopass hyperion/service_account/hyperion.priv.gpg > hyperion.priv
+  #
+  FILE=/tmp/hyperion.priv.gpg
+  if [ -f "$FILE" ]; then
+      echo "#############################################"
+      echo "######### Import hyperion secret ############"
+      echo "#############################################"
+
+      # import secret and ignore error
+      gpg --import $FILE || true
+      # trust with ultimate (5)
+      gpg --no-tty --command-fd 0 --edit-key hyperion <<EOF
+trust
+5
+y
+quit
+EOF
+      echo "#############################################"
+      echo "############## Config Gopass ################"
+      echo "#############################################"
+
+      gopass --yes setup --remote git@ssh.dev.azure.com:v3/payvision/Hyperion/secrets --alias hyperion --name hyperion --email hyperion@payvision.com
+
+  fi
 fi
 
-FILE=/tmp/hyperion.priv
+FILE=/tmp/id_rsa_hyperion
 if [ -f "$FILE" ]; then
   mkdir -p /root/.ssh
   cp $FILE  /root/.ssh/hyperion.priv
